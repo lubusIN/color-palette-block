65f9bc5ff577c784c9b230df4c77ebd05a74e948
GitHub Action <action@github.com>
2026-06-18 11:52:11 +0000

    Add build files for playground preview

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lubusin/blablablocks-color-palette-block',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '20888487acba9ead2ac13daa23e175c7f8ab654a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'lubusin/blablablocks-color-palette-block' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '20888487acba9ead2ac13daa23e175c7f8ab654a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
